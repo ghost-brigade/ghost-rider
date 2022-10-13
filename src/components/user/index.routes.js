e4e78f12b7b43f6a9604f1fd050f840630f31d61
MoulinLouis <moulin.louis93@gmail.com>
2022-10-13 16:11:36 +0200

    add: add middleware with jwt token check

--- src/components/user/index.routes.js
+++ src/components/user/index.routes.js
@@ -2,14 +2,15 @@ import express from "express";
 import UserController from "./controller/UserController.js";
 import UserLoginController from "./controller/UserLoginController.js";
 import UserRegisterController from "./controller/UserRegisterController.js";
+import AuthentificationMiddleware from "../../common/middleware/AuthentificationMiddleware.js";
 
 const router = express.Router();
 const slug = '/user';
 
-router.get(slug + '/', UserController.list);
-router.get(slug + '/:id', UserController.get);
+router.get(slug + '/', AuthentificationMiddleware, UserController.list);
+router.get(slug + '/:id', AuthentificationMiddleware, UserController.get);
 
-router.get('/register', UserRegisterController.register);
-router.get('/login', UserLoginController.login);
+router.post('/register', UserRegisterController.register);
+router.post('/login', UserLoginController.login);
 
 export default router;
