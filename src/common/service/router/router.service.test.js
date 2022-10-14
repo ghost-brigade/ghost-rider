4905c8f07a5dab05a33652ccd06258864db01c7f
Anthony ARJONA <anthony.arjona@outlook.com>
2022-10-15 00:01:18 +0200

    add: router test

---
+++ src/common/service/router/router.service.test.js
@@ -0,0 +1,37 @@
+import path, {dirname} from "path";
+import {fileURLToPath} from "url";
+import RouterService from "../../../../src/common/service/router/router.service.js";
+
+const currentPath = path.join(dirname(fileURLToPath(import.meta.url)));
+
+let obj = {
+  'test': {
+    path: '/test',
+    method: 'get',
+    controller: function () {},
+    auth: true,
+    roles: ['ROLE_USER']
+  },
+  'test2': {
+    path: '/test2',
+    method: 'all',
+    controller: function () {},
+    auth: false,
+    roles: []
+  }
+};
+
+describe('RouterService', () => {
+  test('normal execution', async () => {
+    await RouterService.init(currentPath + '/tests/normal_case').then((routes) => {
+      expect(JSON.stringify(routes)).toMatch(JSON.stringify(obj));
+    });
+  });
+
+  test('no default export', async () => {
+    await expect(RouterService.init(currentPath + '/tests/no_default_export')).rejects.toThrow('The file test.routes.js must export a default routes object');
+  });
+});
+
+
+
