5301707f63b55068af53fbcbbe0cbdc0a275350c
Anthony ARJONA <anthony.arjona@outlook.com>
2022-10-14 22:51:13 +0200

    add: router service

---
+++ src/common/service/router/router.service.js
@@ -0,0 +1,106 @@
+import fs from "fs";
+import {dirname} from "path";
+import {fileURLToPath} from "url";
+import * as path from "path";
+
+class RouterService {
+
+  #baseDir = path.join(dirname(fileURLToPath(import.meta.url)), '..', '..', '..', '/components');
+  routes = {};
+
+  async init(baseDir = this.#baseDir) {
+    return await this.#load(baseDir);
+  }
+
+  /**
+   * Load all file and put them in the routes object
+   * @param baseDir
+   * @returns {Promise<{}>}
+   */
+  #load = async (baseDir) => {
+    return new Promise((resolve, reject) => {
+      fs.readdir(baseDir, async (err, files) => {
+        if (err) {reject(err);}
+        resolve(files);
+      });
+    }).then(async (files) => {
+
+      for (const file of files) {
+        const path = baseDir + '/' + file;
+
+        /**
+         * If the file is a directory, we call the loader again
+         */
+        if (fs.statSync(path).isDirectory()) {
+          await this.#load(path);
+        } else {
+          if (file.endsWith('.routes.js')) {
+            const module = await import(path);
+
+            if (module.default === undefined || module.default === null || typeof module.default !== 'object') {
+              throw new Error('The file ' + file + ' must export a default routes object');
+            }
+
+            this.#validate(Object.entries(module.default));
+            Object.assign(this.routes, module.default);
+          }
+        }
+      }
+
+      return this.routes;
+    });
+  };
+
+  /**
+   * Get all routes from a file
+   * @param {Array} routes
+   */
+  #validate = (routes) => {
+    for (const [key, value] of routes) {
+      /* Prevent duplicate route name */
+      if (Object.keys(this.routes).length > 0 && this.routes[key] !== undefined) {
+        throw new Error('The route ' + key + ' already exists');
+      }
+
+      if (value.path === undefined || value.path === null || value.path === '') {
+        throw new Error('The route ' + key + ' must have a path');
+      }
+
+      if (value.method === undefined || value.method === null || value.method === '') {
+        throw new Error('The route ' + key + ' must have a method');
+      }
+
+      if (['get', 'post', 'all'].includes(value.method.toLowerCase()) === false) {
+        throw new Error("The method " + value.method + " is not allowed please use 'get', 'post' or 'all'");
+      }
+
+      if (value.controller === undefined || value.controller === null || typeof value.controller !== 'function') {
+        throw new Error('The route ' + key + ' must have a controller defined');
+      }
+
+      /**
+       * By default, if auth is not defined we set it to true to force the authentication
+       */
+      if (value.auth === undefined) {
+        value.auth = true;
+      }
+
+      if (typeof value.auth !== 'boolean') {
+        throw new Error('The auth value of route ' + key + ' must be a boolean');
+      }
+
+      /**
+       * By default, if roles is not defined we set it with ROLE_USER role
+       */
+      if (value.roles === undefined) {
+        value.roles = ['ROLE_USER'];
+      }
+
+      if (Array.isArray(value.roles) === false) {
+        throw new Error('The roles value of route ' + key + ' must be an array');
+      }
+    }
+  };
+}
+
+export default new RouterService();
