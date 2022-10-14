93fa2d1972cea012184f03575dad2555f2bc2590
Anthony ARJONA <anthony.arjona@outlook.com>
2022-10-14 21:52:58 +0200

    rm: old file

--- src/common/service/Security/PasswordValidator.js
+++
@@ -1,7 +0,0 @@
-import bcryptjs from "bcryptjs";
-
-const validate = async (password, passwordToCompare) => {
-    return await bcryptjs.compare(password, passwordToCompare);
-};
-
-export {validate};
