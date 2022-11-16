11af8ce03587a09d5322e8633455280e89dfc61d
Anthony ARJONA <anthony.arjona@outlook.com>
2022-11-16 18:46:31 +0100

    add: registration with validation

--- src/components/security/controller/register.controller.js
+++ src/components/security/controller/register.controller.js
@@ -2,8 +2,16 @@ import * as Response from "../../../common/service/Http/Response.js";
 import UserRepository from "../../user/repository/user.repository.js";
 import Controller from "../../../common/controller/controller.js";
 import PasswordService from "../service/security/password.service.js";
+import emailEvent from "../../email/event/email.event.js";
+import Email from "../../email/email.js";
+import UserRegistrationTokenService from "../service/security/userRegistrationToken.service.js";
 
 class UserControllerRegister extends Controller {
+
+  constructor() {
+    super();
+    this.userRepository = new UserRepository();
+  }
 
   /**
    * Register a new user
@@ -12,28 +20,58 @@ class UserControllerRegister extends Controller {
    * @returns {Promise<*>}
    */
   register = async (req, res) => {
-        if (req.body === undefined || req.body.email === undefined || req.body.password === undefined || req.body.name === undefined || req.body.firstname === undefined) {
-            return Response.unprocessableEntity(req, res, "Missing parameters");
-        }
+    if (req.body === undefined || req.body.email === undefined || req.body.password === undefined || req.body.name === undefined || req.body.firstname === undefined) {
+      return Response.unprocessableEntity(req, res, "Missing parameters");
+    }
 
-        const passwordService = new PasswordService();
+    const {email, password, name, firstname} = req.body;
 
-        try {
-            const user = {
-                email: req.body.email,
-                password: await passwordService.hash(req.body.password),
-                name: req.body.name,
-                firstname: req.body.firstname
-            };
+    try {
+      await this.userRepository.findByEmail(email);
+    } catch (e) {
+      return Response.unprocessableEntity(req, res, "Email already used");
+    }
 
-            const userRepository = new UserRepository();
-            const newUser = await userRepository.create(user);
+    const passwordService = new PasswordService();
 
-            return Response.created(req, res, newUser);
-        } catch (err) {
-            return Response.error(req, res, err.message);
-        }
-    };
+    try {
+      const user = {
+        email: email,
+        password: await passwordService.hash(password),
+        name: name,
+        firstname: firstname
+      };
+
+      const newUser = await this.userRepository.create(user);
+
+      const userRegistrationTokenService = new UserRegistrationTokenService(newUser.email);
+      const token = await userRegistrationTokenService.generate();
+
+      emailEvent.emit("send", new Email({to: newUser.email,
+        subject: "Confirmation de votre compte",
+        template: "confirm-account",
+        context: {
+          name: newUser.name,
+          app_name: "Ghost rider",
+          link: req.url + "/register/confirm/" + token
+        }})
+      );
+
+      return Response.created(req, res, newUser);
+    } catch (err) {
+      return Response.error(req, res, err.message);
+    }
+  };
+
+  //TODO : confirm account controller with token function from service/security/userRegistrationToken.service.js
+  /*
+        const userRegistrationTokenService = new UserRegistrationTokenService(email);
+
+        generate token with : await userRegistrationTokenService.generate();
+        verify token with : await userRegistrationTokenService.verify(token);
+   */
+
+  //TODO : test registration
 }
 
 export default new UserControllerRegister();
