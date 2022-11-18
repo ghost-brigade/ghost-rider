b9a98a1db9117109f3b7aabfe2e0e1a32558ac68
Anthony ARJONA <anthony.arjona@outlook.com>
2022-11-18 18:06:22 +0100

    fix: prefix for channel socket

--- src/components/channel/socket/channel.socket.js
+++ src/components/channel/socket/channel.socket.js
@@ -6,18 +6,20 @@ const PREFIX = 'channel:';
 SocketService.io.on('connection', (socket) => {
   socket.on(PREFIX + 'join', async (channelId) => {
     const channelService = new ChannelService(socket);
+
     try {
       await channelService.join(channelId);
     } catch (err) {
-      await socket.emit(channelService.ROOM_PREFIX + 'join:error', {message: err.message});
+      await socket.emit(PREFIX + 'join:error', {message: err.message});
     }
   });
   socket.on(PREFIX + 'leave', async (channelId) => {
     const channelService = new ChannelService(socket);
+
     try {
       await channelService.leave(channelId);
     } catch (err) {
-      await socket.emit(channelService.ROOM_PREFIX + 'leave:error', {message: err.message});
+      await socket.emit(PREFIX + 'leave:error', {message: err.message});
     }
   });
 });
