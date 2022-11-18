12050e17c470d447b8243139c9c7c940e052ac2e
Anthony ARJONA <anthony.arjona@outlook.com>
2022-11-18 20:22:32 +0100

    add: Hierarchy system

--- src/components/security/service/security/Guard.js
+++ src/components/security/service/security/Guard.js
@@ -1,4 +1,9 @@
 class Guard {
+
+  static ROLES_HIERARCHY = {
+    ROLE_ADMIN: ['ROLE_USER'],
+  };
+
   /**
    * Check if the user has role
    * @param {string|array} roles
@@ -14,7 +19,13 @@ class Guard {
       roles = [roles];
     }
 
-    return roles.some(role => user.roles.includes(role));
+    return roles.some((role) => {
+      if (user.roles.includes(role)) {
+        return true;
+      }
+
+      return this.ROLES_HIERARCHY[role]?.some((role) => user.roles.includes(role));
+    });
   }
 }
 
