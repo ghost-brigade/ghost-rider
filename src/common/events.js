6dbbb0e629a65fd4cde57a6abf55026ae91540c9
Anthony ARJONA <anthony.arjona@outlook.com>
2023-01-26 00:14:39 +0100

    add event log

--- src/common/events.js
+++ src/common/events.js
@@ -1,11 +1,19 @@
 import EmailEvent from "../components/email/event/email.event.js";
+import ChabotEvent from "../components/chatbot/event/chabot.event.js";
 
 const events = {
   EmailEvent,
+  ChabotEvent,
 };
 
 const register = () => {
-  if (process.env.NODE_ENV === 'dev') {console.log("events registered");}
+  if (process.env.NODE_ENV === 'dev') {
+    console.log('====== EVENT DEBUG ======');
+    Object.entries(events).forEach(([name, event]) => {
+      console.log(`Registering ${name} : ${event.listenerCount('send')} listeners`);
+    });
+    console.log('======== ====== ========\n');
+  }
 };
 
 export default {
