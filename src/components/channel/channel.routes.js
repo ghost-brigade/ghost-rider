694fbee6cb96c927976fb11c75054145001ee217
Anthony ARJONA <anthony.arjona@outlook.com>
2023-01-31 00:23:22 +0100

    fix routes

--- src/components/channel/channel.routes.js
+++ src/components/channel/channel.routes.js
@@ -8,19 +8,22 @@ export default {
     path: slug ,
     method: 'get',
     controller: ChannelController.findAll,
-    roles: [AuthentificationMiddleware],
+    roles: ['ROLE_USER'],
+    middlewares: [AuthentificationMiddleware],
   },
   'channel_get_item': {
     path: slug + '/:id',
     method: 'get',
     controller: ChannelController.find,
-    roles: [AuthentificationMiddleware],
+    roles: ['ROLE_USER'],
+    middlewares: [AuthentificationMiddleware],
   },
   'channel_post_collection': {
     path: slug,
     method: 'post',
     controller: ChannelController.create,
-    roles: [AuthentificationMiddleware],
+    roles: ['ROLE_ADMIN'],
+    middlewares: [AuthentificationMiddleware],
   },
   'channel_connect_conseiller': {
     path: slug + '/conseiller/:id',
@@ -33,12 +36,14 @@ export default {
     path: slug + '/:id',
     method: 'put',
     controller: ChannelController.update,
-    roles: [AuthentificationMiddleware],
+    roles: ['ROLE_ADMIN'],
+    middlewares: [AuthentificationMiddleware],
   },
   'channel_delete_item': {
     path: slug + '/:id',
     method: 'delete',
     controller: ChannelController.delete,
-    roles: [AuthentificationMiddleware],
+    roles: ['ROLE_ADMIN'],
+    middlewares: [AuthentificationMiddleware],
   }
 };
